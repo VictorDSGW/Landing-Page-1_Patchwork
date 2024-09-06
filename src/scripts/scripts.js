8fbf5cac864bd1c887c0d09c1ea4852ca1f4f01d
Victor DSG <victor.dsgw@gmail.com>
2024-09-06 19:10:23 -0300

    ScrollReveal

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -33,4 +33,29 @@ $(document).ready(function(){
     navItems.removeClass('active');
     $(navItems[activeSectionIndex]).addClass('active');
   });
+  
+  ScrollReveal().reveal('.cta', {
+    origin: 'right',
+    duration: 2500,
+    distance: '50%',
+  })
+
+  ScrollReveal().reveal('.item', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '10%',
+  })
+
+  ScrollReveal().reveal('#testimonial', {
+    origin: 'right',
+    duration: 1500,
+    distance: '15%',
+  })
+
+  ScrollReveal().reveal('input, textarea', {
+    origin: 'bottom',
+    duration: 2000,
+    distance: '20%',
+  })
+
 });
